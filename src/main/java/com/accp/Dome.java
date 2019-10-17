b84c0b6e72db79843a8023b7bb1a7a0ad46290c5
luofuxi <luofuxi@example.com>
2019-10-17 10:43:31 +0800

    第一次提交

---
+++ src/main/java/com/accp/Dome.java
@@ -0,0 +1,10 @@
+package com.accp;
+
+public class Dome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ssss");
+	}
+
+}
---
+++ src/test/java/com/accp/SpringbootGit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
